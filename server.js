fe57e349d19f151e952b063b9f6e9528f1b4cac5
gveindustriaadm-eng <gveindustria.adm@gmail.com>
2026-03-15 20:26:22 -0300

    server.js

--- server.js
+++ server.js
@@ -1,52 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>8 BALL PRO</title>
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const path = require("path");
 
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/main.css">
-<link rel="stylesheet" href="css/orientation_utils.css">
-<link rel="stylesheet" href="css/ios_fullscreen.css">
+const app = express();
+app.use(cors());
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+app.use(express.static(__dirname));
 
-</head>
-
-<body style="background-color:#000;">
-
-<canvas id="canvas" width="1280" height="640"></canvas>
-
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/createjs.min.js"></script>
-<script src="js/howler.min.js"></script>
-
-<script src="js/platform.js"></script>
-<script src="js/screenfull.js"></script>
-
-<script src="js/sprite_lib.js"></script>
-<script src="js/ctl_utils.js"></script>
-<script src="js/settings.js"></script>
-
-<script src="js/CLang.min.js"></script>
-
-<script src="js/CVector2.js"></script>
-<script src="js/CMath.js"></script>
-
-<script src="js/CPreloader.js"></script>
-<script src="js/CMain.js"></script>
-
-<script>
-$(document).ready(function(){
-
-    var oMain = new CMain({
-        audio_enable_on_startup:false,
-        fullscreen:true,
-        check_orientation:true
-    });
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
-</script>
 
-</body>
-</html>
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+let waitingPlayer = null;
+
+io.on("connection", (socket) => {
+  console.log("Jogador conectado:", socket.id);
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
+});
